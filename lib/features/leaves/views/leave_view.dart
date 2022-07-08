7909414be0707b712ae464403a6bd9e8757cf005
sanjit <mehtasanjit202@gmail.com>
2022-07-08 08:48:10 +0545

    cached networking and expanded widget

--- lib/features/leaves/views/leave_view.dart
+++ lib/features/leaves/views/leave_view.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
 class LeaveView extends StatefulWidget {
-  const LeaveView({ Key? key }) : super(key: key);
+  const LeaveView({Key? key}) : super(key: key);
 
   @override
   State<LeaveView> createState() => _LeaveViewState();
@@ -11,10 +13,54 @@ class _LeaveViewState extends State<LeaveView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Leaveview"),
+        automaticallyImplyLeading: false,
+        title: const Text("Leaveview"),
         centerTitle: true,
       ),
-      
+      body: Column(children: [
+        Container(
+          height: 70,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Colors.greenAccent, borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            children: [
+              SizedBox(width: 5,),
+              Expanded(
+                  child: TextFormField(
+                decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(5),
+                    hintText: "search",
+                    labelText: "New",
+                    filled: true,
+                    fillColor: Colors.white),
+              )),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+            ],
+          ),
+        ),
+        Expanded(
+          child: CachedNetworkImage(
+            imageUrl:
+                "https://thumbs.dreamstime.com/z/random-word-concept-cubes-171151178.jpg",
+            height: 200,
+            width: 200,
+            fit: BoxFit.cover,
+            progressIndicatorBuilder: (context, url, progess) => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            errorWidget: (context, url, error) => const Icon(
+              Icons.error,
+            ),
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.blue,
+            )),
+        const Expanded(flex: 2, child: Text("hellow")),
+      ]),
     );
   }
 }
